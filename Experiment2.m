3a41af84d358d4b8dcd054d1d43c01621342398c
Monjur Morshed <monjurmorshedosama75@gmail.com>
2025-10-17 15:33:38 +0600

    Change in 2nd Code

--- Experiment2.m
+++ Experiment2.m
@@ -1,33 +1,14 @@
-% Let x[n]={1,2,3,4,5,6,7,6,5,4,3,2,1}.Here bold number 
-%indicates the 0 index. Determine and plot the following sequences. 
-%y[n]=2x[n?5]?3x[n+4]. 
-clc; clear; close all;
-
 % Given sequence
 x = [1 2 3 4 5 6 7 6 5 4 3 2 1];
-n = -6:6;             % Since middle (7th element) is x[0]
+n = -4:8;  % 0 index at element 5 ? n = -4 to 8
 
-% Define y[n] = 2x[n?5] ? 3x[n+4]
-n_shift1 = n + 5;      % For x[n?5] -> shift right by 5
-n_shift2 = n - 4;      % For x[n+4] -> shift left by 4
+% Compute y[n] = 2x[n-5] - 3x[n+4]
+n_shift = n;
+x_shift_minus5 = [zeros(1,5), x(1:end-5)];
+x_shift_plus4 = [x(5:end), zeros(1,4)];
 
-% Define a common range for n
-n_y = -15:15;
+y = 2*x_shift_minus5 - 3*x_shift_plus4;
 
-% Initialize with zeros
-x_shift1 = zeros(size(n_y));
-x_shift2 = zeros(size(n_y));
-
-% Insert shifted sequences
-x_shift1(ismember(n_y, n_shift1)) = x;
-x_shift2(ismember(n_y, n_shift2)) = x;
-
-% Compute y[n]
-y = 2 * x_shift1 - 3 * x_shift2;
-
-% Plot
-stem(n_y, y, 'filled');
+stem(n_shift, y, 'filled');
 title('y[n] = 2x[n-5] - 3x[n+4]');
-xlabel('n');
-ylabel('Amplitude');
-grid on;
+xlabel('n'); ylabel('Amplitude');
